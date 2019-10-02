1a8c67386cc140f291898c9f684add94ce7f0985
Marissa Carson <mmcarson@coe.edu>
2019-10-02 09:08:32 -0500

    modifying menu

--- togglemenu.js
+++ togglemenu.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     if (document.getElementById("main-nav").style.display == "none") {
-        document.getElementById("main-nav").style.display = "block";
+        document.getElementById("main-nav").style.display = "inline-block";
         document.getElementById("burger").classList.add("open");
     } else {
         document.getElementById("main-nav").style.display = "none";
