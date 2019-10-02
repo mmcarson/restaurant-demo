5388367ba59c39bade0a06c7d5851bee233a088d
Marissa Carson <mmcarson@coe.edu>
2019-10-02 09:37:14 -0500

    gave transition proper height

--- togglemenu.js
+++ togglemenu.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
     if (document.getElementById("main-nav").style.height == "0px") {
-        document.getElementById("main-nav").style.height = "100vh";
+        var numChildren = document.getElementById("main-nav").childNodes.length;
+        document.getElementById("main-nav").style.height = (numChildren * 1.5) + "em";
         document.getElementById("burger").classList.add("open");
     } else {
         document.getElementById("main-nav").style.height = "0px";
