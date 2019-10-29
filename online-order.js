082e458d5bc84f41d42b09f8a42e9e63ecb869d2
Marissa Carson <mmcarson@coe.edu>
2019-10-29 18:38:55 -0500

    in progress table

--- online-order.js
+++ online-order.js
@@ -1,5 +1,14 @@
-window.addEventListener("storage", updateCart);
-
 let updateCart = function() {
+    console.log("called updateCart");
+    // identify the table
+    let cartTable = document.getElementById("cart-content");
+    console.dir(cartTable);
+    // clear the table
 
 }
+
+// this is always called when the page loads
+window.onload = updateCart;
+
+// local storage ONLY applies to another window so this will only matter if you have two windows open
+window.addEventListener("storage", updateCart);
