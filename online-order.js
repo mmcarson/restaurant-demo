4ce6844cb5fa50601ea6ee8beee2f0f7db8afe95
Marissa Carson <mmcarson@coe.edu>
2019-10-30 08:25:11 -0500

    looping through array to add to table and formatted table

--- online-order.js
+++ online-order.js
@@ -1,41 +1,59 @@
+let cartTable;
+
 // regular named function
 function makeCartRow(item, price) {
-    let row = document.createElement("tr");
+    const row = document.createElement("tr");
     // td is table data
-    let itemData = document.createElement("td");
+    const itemData = document.createElement("td");
     itemData.innerText = item;
     row.appendChild(itemData);
-    let priceData = document.createElement("td");
+    const priceData = document.createElement("td");
     priceData.innerText = price;
     row.appendChild(priceData);
     return row;
 }
 
+let exampleArray = [{ item: "soup", price: 3 },
+    { item: "salad", price: 3 },
+    { item: "sandwich", price: 5 }
+]
+
+const rowFromItem = function(value) {
+    cartTable.appendChild(makeCartRow(value.item, '$' + value.price));
+}
+
 // anonymous function as a variable
-let updateCart = function() {
+const updateCart = function() {
+    cartTable = document.getElementById("cart-content"); // step 1: get parent element
     console.log("called updateCart");
     // identify the table
-    let cartTable = document.getElementById("cart-content");
     console.dir(cartTable);
     // clear the table
     cartTable.innerHTML = "";
     // add the headers
-    let headerRow = document.createElement("tr");
-    let itemHeader = document.createElement("th");
-    let amountHeader = document.createElement("th");
-    itemHeader.innerText = "Item";
+    const headerRow = document.createElement("tr"); // step 2: create new element to be child
+    const itemHeader = document.createElement("th"); // also step 2 at the next level down
+    const amountHeader = document.createElement("th");
+    itemHeader.innerText = "Item"; // step 3: create content for new element
     amountHeader.innerText = "Amount";
-    headerRow.appendChild(itemHeader);
+    headerRow.appendChild(itemHeader); // step 4: append child (lower level)
     headerRow.appendChild(amountHeader);
-    cartTable.appendChild(headerRow);
+    cartTable.appendChild(headerRow); // also step 4 (higher level)
     // add the example items
-    cartTable.appendChild(makeCartRow("soup", "$3"));
-    cartTable.appendChild(makeCartRow("salad", "$3"));
-    cartTable.appendChild(makeCartRow("sandwich", "$5"));
+    // cartTable.appendChild(makeCartRow("soup", "$3"));
+    // cartTable.appendChild(makeCartRow("salad", "$3"));
+    // cartTable.appendChild(makeCartRow("sandwich", "$5"));
+    exampleArray.forEach(rowFromItem);
 }
+
+// updateCart = function() {
+//     console.log("3");
+// }
 
 // this is always called when the page loads
 window.onload = updateCart;
+// just calling the function will create an error because the element does not exist
+// updateCart();
 
 // local storage ONLY applies to another window so this will only matter if you have two windows open
-window.addEventListener("storage", updateCart); // not used yet
+// window.addEventListener("storage", updateCart); // not used yet
